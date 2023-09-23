b39cdff19e9f7639b6b1bd809409ef9d8b59aa8e
tatoyoda600 <datrashbasura@gmail.com>
2023-09-23 18:25:59 -0300

    AR dependencies and basic test view

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -8,12 +8,19 @@ android {
     compileSdk = 33
 
     defaultConfig {
+        configurations.all {
+            resolutionStrategy {
+                // emoji2 1.4.0 is imported by some module and requires SDK 34, which is not currently supported
+                // This overrides the import with the 1.3.0 version
+                force("androidx.emoji2:emoji2-views-helper:1.3.0")
+                force("androidx.emoji2:emoji2:1.3.0")
+            }
+        }
         applicationId = "com.pfortbe22bgrupo2.architectapp"
         minSdk = 26
         targetSdk = 33
         versionCode = 1
         versionName = "1.0"
-
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
 
@@ -44,4 +51,7 @@ dependencies {
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
+
+    // ARCore and SceneView (Androidx version of Sceneform: https://github.com/SceneView/sceneview-android)
+    implementation("io.github.sceneview:arsceneview:0.10.2")
 }
