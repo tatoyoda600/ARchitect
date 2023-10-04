6d0d4b3a5d3a223ba6821d09f2bdf8750675dbf7
tatoyoda600 <datrashbasura@gmail.com>
2023-10-04 15:54:30 -0300

    Floor height detection

--- app/src/main/java/com/pfortbe22bgrupo2/architectapp/activities/ARTrackingTest.kt
+++ app/src/main/java/com/pfortbe22bgrupo2/architectapp/activities/ARTrackingTest.kt
@@ -22,6 +22,9 @@ private const val MAX_DISTANCE = 0.1f
 private const val MIN_NEIGHBORS = 3
 private const val CLEAN_UP_CELL_MIN_POINTS = 2 // Cells with less than this many points will be emptied
 private const val CLEAN_UP_CELL_MAX_POINTS = 4 // Cells with more than this many points will be culled
+private const val MIN_FLOOR_POINTS = 20
+private const val CONFIRMED_POINTS_FLOOR_CHECK_STEP = 50
+private const val ONE_POINT_PER_CELL = false // Points that fall into an occupied cell are discarded (Confirmed points will replace unconfirmed points)
 
 class ARTrackingTest: AppCompatActivity() {
     class Point(
@@ -34,12 +37,15 @@ class ARTrackingTest: AppCompatActivity() {
     private var lastFrame: ArFrame? = null
     private var lastExcessCleanUpStep = 0
     private var lastConfirmedCleanUpStep = 0
+    private var lastConfirmedFloorCheckStep = 0
     private val pointIds = mutableListOf<Int>()
     private val points = mutableMapOf<Int,MutableMap<Int,MutableMap<Int,MutableList<Point>>>>()
-
     // Currently new points that have enough neighbors are marked as confirmed
     // Points that get neighbors added afterwards are not added
     private val confirmedPoints = mutableListOf<Point>()
+
+    private var useFloorHeight = false
+    private var floorHeight = Int.MAX_VALUE
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -86,8 +92,19 @@ class ARTrackingTest: AppCompatActivity() {
                             pointIds += pointCloud.ids[i]
                             val point = Point(pointCloud.ids[i], position)
                             if (addPoint(point) >= MIN_NEIGHBORS) {
-                                confirmedPoints += point
-                                Log.w("Confirmed Points: ", position.toString())
+                                if (!useFloorHeight || (position.y * DICT_COORD_ZOOM).toInt() == floorHeight) {
+                                    confirmedPoints += point
+                                    Log.w("Confirmed Points: ", position.toString())
+
+                                    if (confirmedPoints.size > lastConfirmedFloorCheckStep + CONFIRMED_POINTS_FLOOR_CHECK_STEP) {
+                                        lastConfirmedFloorCheckStep += CONFIRMED_POINTS_FLOOR_CHECK_STEP
+                                        var floor = calculateFloorHeight()
+                                        if (floor.second > MIN_FLOOR_POINTS) {
+                                            floorHeight = floor.first
+                                            Log.w("Floor Height: ", floorHeight.toString())
+                                        }
+                                    }
+                                }
                             }
                         }
                     }
@@ -122,13 +139,30 @@ class ARTrackingTest: AppCompatActivity() {
         val x = (position.x * DICT_COORD_ZOOM).toInt()
         val y = (position.y * DICT_COORD_ZOOM).toInt()
         val z = (position.z * DICT_COORD_ZOOM).toInt()
+        val cellPoints = points.get(x)?.get(y)?.get(z)
+
+        // If each cell is limited to 1 point and the cell for this point already contains a point
+        if (ONE_POINT_PER_CELL && cellPoints?.size?:0 > 0) {
+            // If the point in the cell is not a confirmed point
+            if (!pointIds.contains(cellPoints?.get(0)?.id)) {
+                // Remove the point in the cell
+                pointIds.remove(cellPoints?.get(0)?.id)
+                points.get(x)?.get(y)?.get(z)?.removeAt(0)
+            }
+            // If the point in the cell is a confirmed point
+            else {
+                // Discard this new point
+                return -1
+            }
+        }
+
         points.getOrPut(x) { mutableMapOf() }
             .getOrPut(y) { mutableMapOf() }
             .getOrPut(z) { mutableListOf() }
             .add(point)
 
         var neighborCount = -1 // Start at -1 since the point itself will be counted
-        for (p in points.get(x)?.get(y)?.get(z)?: mutableListOf()) {
+        for (p in cellPoints?: mutableListOf()) {
             if ((p.position.xyz - position.xyz).toVector3().length() < MAX_DISTANCE) {
                 neighborCount++
                 if (neighborCount >= MIN_NEIGHBORS) return neighborCount
@@ -138,7 +172,7 @@ class ARTrackingTest: AppCompatActivity() {
         for (scanX in ((position.x - MAX_DISTANCE) * DICT_COORD_ZOOM).toInt()..((position.x + MAX_DISTANCE) * DICT_COORD_ZOOM).toInt()) {
             for (scanY in ((position.y - MAX_DISTANCE) * DICT_COORD_ZOOM).toInt()..((position.y + MAX_DISTANCE) * DICT_COORD_ZOOM).toInt()) {
                 for (scanZ in ((position.z - MAX_DISTANCE) * DICT_COORD_ZOOM).toInt()..((position.z + MAX_DISTANCE) * DICT_COORD_ZOOM).toInt()) {
-                    for (p in points.get(scanX)?.get(scanY)?.get(scanZ)?: mutableListOf()) {
+                    for (p in cellPoints?: mutableListOf()) {
                         if ((p.position.xyz - position.xyz).toVector3().length() < MAX_DISTANCE) {
                             neighborCount++
                             if (neighborCount >= MIN_NEIGHBORS) return neighborCount
@@ -151,6 +185,8 @@ class ARTrackingTest: AppCompatActivity() {
     }
 
     private fun cleanUpExcessPoints(): Int {
+        if (ONE_POINT_PER_CELL) return 0
+
         var cleanUpCount = 0
         // Iterate through every cell in the dictionary
         for (xKey in points.keys) {
@@ -176,6 +212,8 @@ class ARTrackingTest: AppCompatActivity() {
     }
 
     private fun cleanUpConfirmedPoints(): Int {
+        if (ONE_POINT_PER_CELL) return 0
+
         var cleanUpCount = 0
         // Loop through every confirmed point
         for (point in (confirmedPoints.lastIndex?: -1) downTo 0) {
@@ -222,5 +260,68 @@ class ARTrackingTest: AppCompatActivity() {
             }
         }
         return cleanUpCount
+    }
+
+    private fun calculateFloorHeight(): Pair<Int,Int> {
+        var pointsPerHeight = mutableMapOf<Int,Int>()
+        for (p in confirmedPoints) {
+            val y = (p.position.y * DICT_COORD_ZOOM).toInt()
+            if (!pointsPerHeight.containsKey(y)) {
+                pointsPerHeight.put(y, confirmedPointsAtHeight(y))
+            }
+        }
+
+        var curMax: Int = -1
+        var curHeight: Int = Int.MAX_VALUE
+        for (p in pointsPerHeight) {
+            if (p.value > curMax) {
+                curMax = p.value
+                curHeight = p.key
+            }
+            else if (p.value == curMax && p.key < curHeight) {
+                curHeight = p.key
+            }
+        }
+
+        return Pair(curHeight, curMax)
+    }
+
+    private fun confirmedPointsAtHeight(height: Int): Int {
+        var floorChance = 0
+        // Loop through every confirmed point
+        for (point in (confirmedPoints.lastIndex?: -1) downTo 0) {
+            // If the point's height is the same as the provided height
+            if ((confirmedPoints[point].position.y * DICT_COORD_ZOOM).toInt() == height) {
+                floorChance++
+            }
+        }
+        return floorChance
+    }
+
+    private fun enableFloorHeight() {
+        useFloorHeight = true
+        clearExcessPoints()
+
+        // Loop through every confirmed point
+        for (point in (confirmedPoints.lastIndex?: -1) downTo 0) {
+            val position = confirmedPoints[point].position
+            val x = (position.x * DICT_COORD_ZOOM).toInt()
+            val y = (position.y * DICT_COORD_ZOOM).toInt()
+            val z = (position.z * DICT_COORD_ZOOM).toInt()
+            val cellPoints = points.get(x)?.get(y)?.get(z)
+
+            // If this point is not at floorHeight
+            if (y != floorHeight) {
+                for (p in (cellPoints?.lastIndex?: -1)downTo 0) {
+                    if (confirmedPoints[point].id == cellPoints?.get(p)?.id) {
+                        // Remove the point
+                        pointIds.remove(confirmedPoints[point].id)
+                        points.get(x)?.get(y)?.get(z)?.removeAt(p)
+                        confirmedPoints.removeAt(point)
+                        break
+                    }
+                }
+            }
+        }
     }
 }
