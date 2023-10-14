da8e1652246e6d8634528908074cb6355df54343
JuanFurgi <juanpifurgi1@gmail.com>
2023-10-14 01:34:55 -0300

    Renderizado 3D en Android

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -5,7 +5,7 @@ plugins {
 
 android {
     namespace = "com.pfortbe22bgrupo2.architectapp"
-    compileSdk = 33
+    compileSdk = 34
 
     defaultConfig {
         configurations.all {
@@ -17,8 +17,8 @@ android {
             }
         }
         applicationId = "com.pfortbe22bgrupo2.architectapp"
-        minSdk = 26
-        targetSdk = 33
+        minSdk = 28
+        targetSdk = 34
         versionCode = 1
         versionName = "1.0"
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
@@ -40,18 +40,40 @@ android {
     kotlinOptions {
         jvmTarget = "1.8"
     }
+    buildFeatures{
+        viewBinding = true
+    }
 }
 
 dependencies {
 
-    implementation("androidx.core:core-ktx:1.9.0")
+//    implementation("androidx.core:core-ktx:1.9.0")
     implementation("androidx.appcompat:appcompat:1.6.1")
     implementation("com.google.android.material:material:1.9.0")
     implementation("androidx.constraintlayout:constraintlayout:2.1.4")
+    implementation("androidx.navigation:navigation-ui-ktx:2.7.4")
+    implementation("androidx.navigation:navigation-fragment-ktx:2.7.4")
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
 
     // ARCore and SceneView (Androidx version of Sceneform: https://github.com/SceneView/sceneview-android)
     implementation("io.github.sceneview:arsceneview:0.10.2")
+
+    //Coroutines
+    api("org.jetbrains.kotlin:kotlin-stdlib:")
+    api("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.6.4")
+    api("org.jetbrains.kotlinx:kotlinx-coroutines-android:1.6.4")
+
+    // AndroidX
+    api("androidx.core:core-ktx:1.10.1")
+    api("androidx.appcompat:appcompat:1.6.1")
+    api ("androidx.fragment:fragment-ktx:1.6.1")
+
+    // SceneView
+//    implementation("io.github.sceneview:sceneview:1.0.12")
+
+    testImplementation("junit:junit:4.13.2")
+    androidTestImplementation("androidx.test.ext:junit:1.1.5")
+    androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
 }
---
+++ app/src/main/java/com/pfortbe22bgrupo2/architectapp/AR_Model.kt
@@ -0,0 +1,48 @@
+package com.pfortbe22bgrupo2.architectapp
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.google.android.material.bottomnavigation.BottomNavigationView
+import com.google.android.material.floatingactionbutton.ExtendedFloatingActionButton
+import com.pfortbe22bgrupo2.architectapp.databinding.ActivityArModelBinding
+import io.github.sceneview.ar.ArSceneView
+
+class AR_Model : AppCompatActivity(R.layout.activity_ar_model), Render3D {
+
+    private lateinit var binding: ActivityArModelBinding
+    private lateinit var bottomNavigationView: BottomNavigationView
+    private lateinit var sceneView: ArSceneView
+    private lateinit var placeButton: ExtendedFloatingActionButton
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityArModelBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        bottomNavigationView = binding.bottomNav
+
+        bottomNavigationView.selectedItemId = R.id.Item3D
+
+        binding.bottomNav.setOnItemSelectedListener {
+
+            when(it.itemId){
+                R.id.ItemHome -> {
+                    startActivity(Intent(applicationContext, MainActivity::class.java))
+                    overridePendingTransition(R.anim.slide_in_right, R.anim.slide_out_left)
+                    finish()
+                }
+
+                R.id.Item3D -> {}
+
+            }
+            true
+        }
+
+        sceneView = binding.arSceneView
+
+        placeButton = binding.extendedFloatingActionButton
+
+        render(sceneView, placeButton)
+    }
+}
