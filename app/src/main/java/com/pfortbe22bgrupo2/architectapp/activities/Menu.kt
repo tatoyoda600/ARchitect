9aa955354a2be07a283e38dfe7567aea4cda09e6
Malena Gonzalez <63439439+malenagonzalez@users.noreply.github.com>
2023-10-29 00:14:45 -0300

    estilo filtros para el catalogo de productos

--- app/src/main/java/com/pfortbe22bgrupo2/architectapp/activities/Menu.kt
+++ app/src/main/java/com/pfortbe22bgrupo2/architectapp/activities/Menu.kt
@@ -4,7 +4,7 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.pfortbe22bgrupo2.architectapp.R
 
-class menu : AppCompatActivity() {
+class Menu : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_menu)
