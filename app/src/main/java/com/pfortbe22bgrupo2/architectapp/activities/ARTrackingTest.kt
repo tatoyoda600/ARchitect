63ddd7671d1790df6f270954ca08207f754af21b
tatoyoda600 <datrashbasura@gmail.com>
2023-10-29 22:10:21 -0300

    Algorithmic floor filling, plus some code separation and clean up

--- app/src/main/java/com/pfortbe22bgrupo2/architectapp/activities/ARTrackingTest.kt
+++ app/src/main/java/com/pfortbe22bgrupo2/architectapp/activities/ARTrackingTest.kt
@@ -2,61 +2,33 @@ package com.pfortbe22bgrupo2.architectapp.activities
 //https://github.com/SceneView/sceneview-android/blob/main/samples/ar-model-viewer/src/main/java/io/github/sceneview/sample/armodelviewer/MainActivity.kt
 
 import android.os.Bundle
-import android.util.Log
 import androidx.appcompat.app.AppCompatActivity
 import com.pfortbe22bgrupo2.architectapp.databinding.ActivityArtrackingTestBinding
-import com.pfortbe22bgrupo2.architectapp.utilities.ARTracking
-import com.pfortbe22bgrupo2.architectapp.utilities.Floor
-import com.pfortbe22bgrupo2.architectapp.utilities.Outline
+import com.pfortbe22bgrupo2.architectapp.utilities.DefaultARTracking
 import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.Job
-import kotlinx.coroutines.launch
 
 class ARTrackingTest: AppCompatActivity() {
     val scope = CoroutineScope(Job() + Dispatchers.Main)
     lateinit var binding: ActivityArtrackingTestBinding
-    lateinit var arTracking: ARTracking
+    lateinit var arTracking: DefaultARTracking
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityArtrackingTestBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
-        arTracking = ARTracking(5, binding.sceneView)
-        arTracking.setup(
-            arTracking::pointScanning,
-            arTracking::onConfirmedPoint,
-            fun(){
-                binding.confirmBtn.isEnabled = true
-            }
-        )
+        arTracking = DefaultARTracking(5, binding.sceneView, fun(){
+            binding.confirmBtn.isEnabled = true
+        })
 
         binding.rescanBtn.setOnClickListener {
             arTracking.reset()
         }
 
         binding.confirmBtn.setOnClickListener {
-            scope.launch {
-                // TODO("FIGURE OUT HOW TO GET THIS TO TRULY RUN ASYNCHRONOUSLY")
-                val outlines: Outline = arTracking.getFloorOutline()
-
-                if (outlines.points.size == 1) {
-                    // Completed outline
-                    for (point in outlines.points[0]) {
-                        arTracking.colorPoint(point)
-                    }
-                    val floor: Floor = arTracking.calculateFloorGrid(outlines)
-                }
-                else {
-                    // Incomplete outline
-                    for (outline in outlines.points) {
-                        for (point in outline) {
-                            arTracking.colorPoint(point)
-                        }
-                    }
-                }
-            }
+            arTracking.confirm()
         }
     }
 }
---
+++ app/src/main/java/com/pfortbe22bgrupo2/architectapp/types/Point.kt
@@ -0,0 +1,8 @@
+package com.pfortbe22bgrupo2.architectapp.types
+
+import io.github.sceneview.math.Position
+
+open class Point(
+    val id: Int,
+    val position: Position
+)
